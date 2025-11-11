63357eacde809e71239413a1c86ca77869f631b5
sucre330 <330675022@qq.com>
2025-11-12 07:56:42 +0800

    use arrary

--- Test/HelloWorld.sol
+++ Test/HelloWorld.sol
@@ -10,18 +10,25 @@ contract HelloWorld {
         address addr;
     }
 
-    
-    function sayHello() public view  returns ( string memory ) {
+    Info[] infos;
+
+    function sayHello(uint256 _id) public view returns (string memory) {
+        for(uint256 i = 0;i < infos.length; i++ ){
+            if(infos[i].id == _id){
+                return appendMessage(infos[i].phrase);
+            }
+        }
+
         return appendMessage(message);
     }
 
-    function setMessage(string memory _message) public {
-        message = _message;
+    function setMessage(string memory _message, uint256 _id) public {
+        Info memory info = Info(_message, _id, msg.sender);
+        infos.push(info);
+        // message = _message;
     }
 
-    function appendMessage(string memory _message) public pure  returns (string memory) {
-       return  string.concat(_message," welcome");
+    function appendMessage(string memory _message) public pure returns (string memory) {
+        return string.concat(_message, " welcome");
     }
-
 }
-
